113d508a0fb6feadedc92c96b435817e0407f47d
MostafaIn <mostafa.hazareh@integrify.fi>
2019-05-02 12:05:13 +0300

    Updates

---
+++ precache-manifest.99dc6dcaed10a6a148dc640f6e36f868.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "802e94025d17e0ad4d54e2958da9c559",
+    "url": "/github.com/memeGenerator-miniReactProject/index.html"
+  },
+  {
+    "revision": "811a8459fb3068f478cc",
+    "url": "/github.com/memeGenerator-miniReactProject/static/css/main.e2287294.chunk.css"
+  },
+  {
+    "revision": "0f19e2518a3a17b37f23",
+    "url": "/github.com/memeGenerator-miniReactProject/static/js/2.e2adb5f9.chunk.js"
+  },
+  {
+    "revision": "811a8459fb3068f478cc",
+    "url": "/github.com/memeGenerator-miniReactProject/static/js/main.8af31ae8.chunk.js"
+  },
+  {
+    "revision": "499aa87f7aef4798e239",
+    "url": "/github.com/memeGenerator-miniReactProject/static/js/runtime~main.ece73e2c.js"
+  }
+]);
